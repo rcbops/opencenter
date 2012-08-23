005e26e5250bfe9dc9bdd1c9577326e2b56679e5
galstrom21 <jshepher@rackspace.com>
2012-08-23 15:59:31 -0500

    completed cluster CRUD

--- roush.py
+++ roush.py
@@ -105,12 +105,81 @@ def role_by_id(role_id):
     return resp
 
 
-@app.route('/clusters', methods=['GET'])
+@app.route('/clusters', methods=['GET', 'POST'])
 def list_clusters():
-    cluster_list = dict(clusters=[dict((c, getattr(r, c))
-                        for c in r.__table__.columns.keys())
-                        for r in Clusters.query.all()])
-    resp = jsonify(cluster_list)
+    if request.method == 'POST':
+        if 'name' in request.json:
+            name = request.json['name']
+            desc = None
+            if 'description' in request.json:
+                desc = request.json['description']
+            cluster = Clusters(name=name, description=desc)
+            db_session.add(cluster)
+            try:
+                db_session.commit()
+                msg = {'status': 201, 'message': 'Cluster Created',
+                           'cluster': dict((c, getattr(cluster, c))
+                                        for c in cluster.__table__.columns.keys()),
+                           'ref': url_for('cluster_by_id', cluster_id=cluster.id)}
+                resp = jsonify(msg)
+                resp.status_code = 201
+            except IntegrityError, e:
+                msg = {'status': 500, "message": e.message}
+                resp = jsonify(msg)
+                resp.status_code = 500
+        else:
+            msg = {'status': 400, "message": "Attribute 'name' was not provided"}
+            resp = jsonify(msg)
+            resp.status_code = 400
+    else:
+        cluster_list = dict(clusters=[dict((c, getattr(r, c))
+                         for c in r.__table__.columns.keys())
+                         for r in Clusters.query.all()])
+        resp = jsonify(cluster_list)
+    return resp
+
+@app.route('/clusters/<cluster_id>', methods=['GET', 'PUT', 'DELETE', 'PATCH'])
+def cluster_by_id(cluster_id):
+    if request.method == 'PATCH' or request.method == 'POST':
+        message = {
+            'status': 501,
+            'message': 'Not Implemented'
+        }
+        resp = jsonify(message)
+        resp.status_code = 501
+    elif request.method == 'PUT':
+        r = Clusters.query.filter_by(id=cluster_id).first()
+        if 'name' in request.json:
+            r.name = request.json['name']
+        if 'description' in request.json:
+            r.description = request.json['description']
+        #TODO(shep): this is an un-excepted db call
+        db_session.commit()
+        resp = jsonify(dict((c, getattr(r, c))
+                       for c in r.__table__.columns.keys()))
+    elif request.method == 'DELETE':
+        r = Clusters.query.filter_by(id=cluster_id).first()
+        try:
+            db_session.delete(r)
+            db_session.commit()
+            msg = {'status': 200, 'message': 'Role deleted'}
+            resp = jsonify(msg)
+            resp.status_code = 200
+        except UnmappedInstanceError, e:
+            msg = {'status': 404, 'message': 'Resource not found',
+                   'cluster': {'id': cluster_id}}
+            resp = jsonify(msg)
+            resp.status_code = 404
+    else:
+        r = Clusters.query.filter_by(id=cluster_id).first()
+        if r is None:
+            msg = {'status': 404, 'message': 'Resource not found',
+                   'cluster': {'id': cluster_id}}
+            resp = jsonify(msg)
+            resp.status_code = 404
+        else:
+            resp = jsonify(dict((c, getattr(r, c))
+                           for c in r.__table__.columns.keys()))
     return resp
 
 
@@ -119,12 +188,10 @@ def node():
     if request.method == 'POST':
         hostname = request.json['hostname']
 
-        # Grab role_id from payload
         role_id = None
         if 'role_id' in request.json:
             role_id = request.json['role_id']
 
-        # Grab cluster_id from payload
         cluster_id = None
         if 'cluster_id' in request.json:
             cluster_id = request.json['cluster_id']
