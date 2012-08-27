2af0e893c918439d2603a4278d48448f9efade69
William Kelly <the.william.kelly@gmail.com>
2012-08-26 20:54:28 -0500

    Modify test_clusters to fit what I think shep had in mind

--- tests/test_clusters.py
+++ tests/test_clusters.py
@@ -7,6 +7,7 @@ import string
 import unittest
 import tempfile
 from test_roush import RoushTestCase
+from setup import RoushTest
 import webapp
 
 
@@ -14,66 +15,58 @@ def _randomStr(size):
     return "".join(random.choice(string.ascii_lowercase) for x in range(size))
 
 
-class ClusterCRUDTestCase(RoushTestCase):
+class ClusterTestCase(RoushTest):
 
-    def test_cluster_crud(self):
-        tmp_name = _randomStr(10)
-        tmp_description = "lorem ipsum"
+    @classmethod
+    def setup(cls):
+         # Create a cluster
+        cls.cluster_name = _randomStr(10)
+        cls.cluster_desc = _randomStr(30)
+        cluster_data = {"name": cls.cluster_name, 
+                            "description": cls.cluster_desc}
+        tmp = cls.app.post('/clusters/', data=json.dumps(cluster_data), 
+                            content_type='application/json')
+        assert tmp.status_code == 201,\
+            "Unable to create cluster %s" % cluster_name
+        cls.cluster_json = json.loads(tmp.data)
+        cls.cluster_id = cls.cluster_json['cluster']['id']
 
-        # create a new cluster
-        cluster = {"name": tmp_name, "description": tmp_description}
-        resp = self.app.post('/clusters/', data=json.dumps(cluster), content_type='application/json')
-        # pprint(resp)
-        self.assertEqual(resp.status_code, 201)
-        data = json.loads(resp.data)
+    @classmethod
+    def cleanup(cls):
+        # Delete our test cluster
+        tmp = cls.app.delete('/clusters/%s' % cls.cluster_id)
+        assert tmp.status_code == 200, "Status code %s is not 200" % (
+            tmp.status_code)
+        data = json.loads(tmp.data)
+        assert data['status'] == tmp.status_code,\
+            "Status %s returned in data does not match response code %s" % (
+                data['status'], tmp.status_code)
+        assert data['message'] == 'Cluster deleted',\
+            "Message %s is not Cluster deleted" % (data['message'])
 
-        # make sure the cluster was created
-        resp = self.app.get('/clusters/%s' % data['cluster']['id'])
-        tmp = json.loads(resp.data)
-        self.assertEqual(resp.status_code, 200)
-        self.assertEqual(tmp['id'], data['cluster']['id'])
-        self.assertEqual(tmp['name'], tmp_name)
-        self.assertEqual(tmp['description'], tmp_description)
-
+    def test_create_cluster(self):
+        #cluster is created in setup.  We should verify it is created
+        #as expected.
+        response = self.app.get("/clusters/%s" % (self.cluster_id))
+        cluster = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.cluster_id, cluster['id'])
+        self.assertEqual(self.cluster_name, cluster['name'])
+        self.assertEqual(self.cluster_desc, cluster['description'])
+        
+    def test_update_cluster(self):
         # update cluster attributes
         new_desc = "updated description"
         new_cluster = {"description": new_desc}
-        resp = self.app.put('/clusters/%s' % data['cluster']['id'], data=json.dumps(new_cluster), content_type='application/json')
+        resp = self.app.put('/clusters/%s' % self.cluster_id, 
+                            data=json.dumps(new_cluster), 
+                            content_type='application/json')
         self.assertEqual(resp.status_code, 200)
         tmp_data = json.loads(resp.data)
         self.assertEqual(tmp_data['description'], new_desc)
 
-        # clean up the cluster
-        resp = self.app.delete('/clusters/%s' % data['cluster']['id'])
-        self.assertEqual(resp.status_code, 200)
-        tmp = json.loads(resp.data)
-        self.assertEqual(tmp['status'], 200)
-        self.assertEqual(tmp['message'], 'Cluster deleted')
 
-
-# class ClusterTestCase(unittest.TestCase):
-
-#     @classmethod
-#     def setUpClass(self):
-#         # This has to be set to expose tracebacks
-#         foo = webapp.Thing(configfile='local.conf', debug = True)
-#         self.app = foo.test_client()
-#         # roush.app.testing = True
-#         # self.app = roush.app.test_client()
-#         # Create a cluster
-#         self.cluster_name = _randomStr(10)
-#         self.cluster_desc = _randomStr(30)
-#         self.cluster_data = {"name": self.cluster_name, "description": self.cluster_desc}
-#         tmp = self.app.post('/clusters/', data=json.dumps(self.cluster_data), content_type='application/json')
-#         self.cluster_json = json.loads(tmp.data)
-#         self.cluster_id = self.cluster_json['cluster']['id']
-
-#     @classmethod
-#     def tearDownClass(self):
-#         tmp = self.app.delete('/cluster/%s' % self.cluster_id)
-
-#     def test_blah(self):
-#         pass
-
+        
+                             
 if __name__ == '__main__':
     unittest.main()
