676f974d2cc33484b410e30e7ad80b3290224fbb
galstrom21 <jshepher@rackspace.com>
2012-08-27 12:45:51 -0500

    tests/setup.py needs to instantiate the db as well

--- tests/setup.py
+++ tests/setup.py
@@ -1,5 +1,6 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 import unittest
+from db.database import init_db
 import webapp
 
 
@@ -8,6 +9,7 @@ class RoushTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         foo = webapp.Thing("roush", configfile='local.conf', debug=True)
+        init_db(foo.config['database_uri']) 
         cls.app = foo.test_client()
         try:
             print "Setting up testcase (%s) ... " % (cls.__name__),
