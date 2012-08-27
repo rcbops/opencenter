d76d05d46cd0566da6666a84f053b4159581244e
galstrom21 <jshepher@rackspace.com>
2012-08-26 23:10:15 -0500

    Trying to rework tests/test_roles

--- tests/test_roles.py
+++ tests/test_roles.py
@@ -1,53 +1,60 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 import json
 import os
+from pprint import pprint
 import random
-import roush
 import string
 import unittest
 import tempfile
-from test_roush import RoushTestCase
-from pprint import pprint
+
+import webapp
 
 
 def _randomStr(size):
     return "".join(random.choice(string.ascii_lowercase) for x in range(size))
 
 
-class RoleCRUDTestCase(RoushTestCase):
+class RoleCRUDTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        foo = webapp.Thing("roush", configfile='local.conf', debug=True)
+        self.app = foo.test_client()
+        self.role_name = _randomStr(10)
+        self.role_desc = _randomStr(30)
+        self.content_type = 'application/json'
 
     def test_role_crud(self):
-        tmp_name = _randomStr(10)
-        tmp_description = "lorem ipsum"
-
         # create a new role
-        role = {"name": tmp_name, "description": tmp_description}
+        role = {'name': self.role_name,
+                'description': self.role_desc}
         resp = self.app.post('/roles/', data=json.dumps(role),
-                             content_type='application/json')
-#        pprint([resp.data, resp.headers])
+                             content_type=self.content_type)
         self.assertEqual(resp.status_code, 201)
         data = json.loads(resp.data)
 
         # make sure the role was created
-        resp = self.app.get('/roles/%s' % data['role']['id'])
+        resp = self.app.get(data['ref'],
+                            content_type=self.content_type)
         tmp = json.loads(resp.data)
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(tmp['id'], data['role']['id'])
-        self.assertEqual(tmp['name'], tmp_name)
-        self.assertEqual(tmp['description'], tmp_description)
+        self.assertEqual(tmp['name'], self.role_name)
+        self.assertEqual(tmp['description'], self.role_desc)
 
         # update role attributes
-        new_desc = "updated description"
+        new_desc = _randomStr(30)
         new_role = {"description": new_desc}
-        resp = self.app.put('/roles/%s' % data['role']['id'],
+        resp = self.app.put(data['ref'],
                             data=json.dumps(new_role),
-                            content_type='application/json')
+                            content_type=self.content_type)
         self.assertEqual(resp.status_code, 200)
         tmp_data = json.loads(resp.data)
         self.assertEqual(tmp_data['description'], new_desc)
+        self.assertNotEqual(tmp_data['description'], self.role_desc)
 
         # clean up the role
-        resp = self.app.delete('/roles/%s' % data['role']['id'])
+        resp = self.app.delete(data['ref'],
+                               content_type=self.content_type)
         self.assertEqual(resp.status_code, 200)
         tmp = json.loads(resp.data)
         self.assertEqual(tmp['status'], 200)
@@ -58,7 +65,6 @@ class RoleCRUDTestCase(RoushTestCase):
 
 #    @classmethod
 #    def setUpClass(self):
-
         # roush.app.testing = True
         # self.app = roush.app.test_client()
         # Create a role
