7648671316bb2c44d2fa267a0ff64be47c7d1dfd
galstrom21 <jshepher@rackspace.com>
2012-08-27 12:33:17 -0500

    Getting db.database to create the engine based of off config variables in roush.py

--- db/database.py
+++ db/database.py
@@ -1,15 +1,18 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker, create_session
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///roush.db', convert_unicode=True)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
+# engine = create_engine('sqlite:///roush.db', convert_unicode=True)
+engine = None
+db_session = scoped_session(lambda: create_session(autocommit=False,
+                                                   autoflush=False,
+                                                   bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
 
 
-def init_db():
+def init_db(uri, **kwargs):
+    global engine
+    engine = create_engine(uri, **kwargs)
     import models
     Base.metadata.create_all(bind=engine)
--- roush.py
+++ roush.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+from db.database import init_db
 from webapp import Thing
 
 if __name__ == '__main__':
     foo = Thing("roush", configfile='local.conf', debug=True)
+    init_db(foo.config['database_uri'])
     foo.run()
