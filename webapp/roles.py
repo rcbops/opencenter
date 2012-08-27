c3fa3030a8511588d68cb5fb01265b0ed7ea6b2d
galstrom21 <jshepher@rackspace.com>
2012-08-27 10:50:44 -0500

    adding error_handler for 409

--- webapp/roles.py
+++ webapp/roles.py
@@ -9,6 +9,8 @@ from sqlalchemy.orm.exc import UnmappedInstanceError
 from db.database import db_session
 from db.models import Nodes, Roles, Clusters
 
+from pprint import pprint
+
 roles = Blueprint('roles', __name__)
 
 
@@ -34,9 +36,7 @@ def list_roles():
                                                                  role_id=role.id)
                 resp.status_code = 201
             except IntegrityError, e:
-                msg = {'status': 500, "message": e.message}
-                resp = jsonify(msg)
-                resp.status_code = 500
+                return http_confict(e)
         else:
             return bad_request("name")
         return resp
@@ -105,9 +105,16 @@ def not_implemented(error=None):
 
 @roles.errorhandler(400)
 def bad_request(error=None):
-    print "ERROR: %s" % error
     msg = {'status': 400,
            'message': "Attribute '%s' was not provided" % error}
     resp = jsonify(msg)
     resp.status_code = 400
+    return resp
+
+@roles.errorhandler(409)
+def http_confict(error=None):
+    pprint(error)
+    msg = {'status': 409, "message": error.message}
+    resp = jsonify(msg)
+    resp.status_code = 409
     return resp
