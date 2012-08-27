5e00a2efdea1bd954c9e036d4e78733c5c3669ad
galstrom21 <jshepher@rackspace.com>
2012-08-26 21:45:46 -0500

    fixing pep8 violations

--- tests/test_roles.py
+++ tests/test_roles.py
@@ -9,6 +9,7 @@ import tempfile
 from test_roush import RoushTestCase
 from pprint import pprint
 
+
 def _randomStr(size):
     return "".join(random.choice(string.ascii_lowercase) for x in range(size))
 
@@ -21,7 +22,8 @@ class RoleCRUDTestCase(RoushTestCase):
 
         # create a new role
         role = {"name": tmp_name, "description": tmp_description}
-        resp = self.app.post('/roles/', data=json.dumps(role), content_type='application/json')
+        resp = self.app.post('/roles/', data=json.dumps(role),
+                             content_type='application/json')
 #        pprint([resp.data, resp.headers])
         self.assertEqual(resp.status_code, 201)
         data = json.loads(resp.data)
@@ -37,7 +39,9 @@ class RoleCRUDTestCase(RoushTestCase):
         # update role attributes
         new_desc = "updated description"
         new_role = {"description": new_desc}
-        resp = self.app.put('/roles/%s' % data['role']['id'], data=json.dumps(new_role), content_type='application/json')
+        resp = self.app.put('/roles/%s' % data['role']['id'],
+                            data=json.dumps(new_role),
+                            content_type='application/json')
         self.assertEqual(resp.status_code, 200)
         tmp_data = json.loads(resp.data)
         self.assertEqual(tmp_data['description'], new_desc)
@@ -60,8 +64,11 @@ class RoleCRUDTestCase(RoushTestCase):
         # Create a role
     #     self.role_name = _randomStr(10)
     #     self.role_desc = _randomStr(30)
-    #     self.role_data = {"name": self.role_name, "description": self.role_desc}
-    #     tmp = self.app.post('/roles', data=json.dumps(self.role_data), content_type='application/json')
+    #     self.role_data = {
+    #         "name": self.role_name,
+    #         "description": self.role_desc}
+    #     tmp = self.app.post('/roles', data=json.dumps(self.role_data),
+    #                         content_type='application/json')
     #     self.role_json = json.loads(tmp.data)
     #     self.role_id = self.role_json['role']['id']
 
--- tests/test_roush.py
+++ tests/test_roush.py
@@ -2,16 +2,15 @@
 import unittest
 import webapp
 
+
 class RoushTestCase(unittest.TestCase):
 
     def setUp(self):
         # This has to be set to expose tracebacks
         # roush.app.testing = True
         # self.app = roush.app.test_client()
-        foo = webapp.Thing("roush", configfile='local.conf', debug = True)
+        foo = webapp.Thing('roush', configfile='local.conf', debug=True)
         self.app = foo.test_client()
-
 
     def tearDown(self):
         pass
-
