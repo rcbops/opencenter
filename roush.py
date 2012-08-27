ae2b7b9cd94f507c2cac182ac50213964d09113b
wilk <william.kelly@rackspace.com>
2012-08-27 11:18:58 -0500

    Maybe I should commit roush

--- roush.py
+++ roush.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
 from webapp import Thing
-import wilkerror
 
 if __name__ == '__main__':
     foo = Thing("roush", configfile='local.conf', debug=True)
-    wilkerror.setup_errors(foo)
     foo.run()
