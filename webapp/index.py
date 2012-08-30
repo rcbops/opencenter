0e86ab073ffe4cd0aabdcfdb0eea9199e690adc3
galstrom21 <jshepher@rackspace.com>
2012-08-30 17:34:52 -0500

    Updating service catalog to appease ron

--- webapp/index.py
+++ webapp/index.py
@@ -14,7 +14,8 @@ def list_index():
         'resources': {
             'clusters': {'url': request.url + 'clusters/'},
             'nodes': {'url': request.url + 'nodes/'},
-            'roles': {'url': request.url + 'roles/'}}}
+            'roles': {'url': request.url + 'roles/'}},
+        'there_is_not_enough_keystone_up_in_this_bitch': True}
     resp = jsonify(msg)
     resp.status_code = 200
     return resp
