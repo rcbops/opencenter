d47e3f91118b100c520fe40da9e7e3d389099b3b
rpedde <ron@pedde.com>
2012-09-28 17:28:41 -0500

    fix fk travesty

--- db/api.py
+++ db/api.py
@@ -1,5 +1,7 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
+from itertools import islice
+
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import UnmappedInstanceError
 from sqlalchemy.sql import and_, or_
@@ -31,7 +33,7 @@ def _model_get_schema(model):
         fields[k]['unique'] = cols[k].unique or cols[k].primary_key
 
         if len(cols[k].foreign_keys) > 0:
-            fields[k]['fk'] = cols[k].foreign_keys.pop().name
+            fields[k]['fk'] = list(cols[k].foreign_keys)[0].target_fullname
 
     return {'schema': fields }
 
