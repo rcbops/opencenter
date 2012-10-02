c43918eccb574f32ce8a53d86ef0cc90a70ac47a
rpedde <ron@pedde.com>
2012-10-02 17:16:58 -0500

    NOW, WITH EXTRA PEP8

--- db/migrate_repo/versions/001_initial_data.py
+++ db/migrate_repo/versions/001_initial_data.py
@@ -21,37 +21,40 @@ def upgrade(migrate_engine):
 
     adventures = [
         {'name': 'install chef',
-         'dsl': json.dumps(
-                {'start_state': 's1',
-                 'states': {
-                        's1': {'action': 'run_task',
-                               'parameters': {'action': 'install_chef'},
-                               'on_success': 's2' }
-                        's2': {'action': 'set_backend',
-                               'parameters': {'backend': 'chef-client',
-                                              'backend_state': 'installed'}}}}),
+         'dsl': json.dumps({'start_state': 's1',
+                            'states': {
+                                's1': {'advance': 'run_task',
+                                       'parameters': {
+                                           'action': 'install_chef'},
+                                       'on_success': 's2'},
+                                's2': {'advance': 'set_backend',
+                                       'parameters': {
+                                           'backend': 'chef-client',
+                                           'backend_state': 'installed'}}}}),
          'language': 'json',
          'backend': 'unprovisioned',
          'backend_state': 'unknown'},
         {'name': 'run chef',
-         'dsl': json.dumps(
-                {'start_state': 's1',
-                 'states': {
-                        's1': {'action': 'run_task',
-                               'parameters': {'action': 'run_chef'}}}}),
+         'dsl': json.dumps({'start_state': 's1',
+                            'states': {'s1':
+                                       {'advance': 'run_task',
+                                        'parameters': {'action':
+                                                       'run_chef'}}}}),
          'language': 'json',
          'backend': 'chef-client',
          'backend_state': 'installed'},
         {'name': 'install chef server',
-         'dsl':  json.dumps(
-                {'start_state': 's1',
-                 'states': {
-                        's1': {'action': 'run_task',
-                               'parameters': {'action': 'install_chef_server'},
-                               'on_success': 's2'},
-                        's2': {'action': 'set_backend',
-                               'parameters': {'backend': 'chef-server',
-                                              'backend_state': 'installed'}}}}),
+         'dsl':  json.dumps({'start_state': 's1',
+                             'states': {'s1':
+                                        {'advance': 'run_task',
+                                         'parameters': {'action':
+                                                        'install_chef_server'},
+                                         'on_success': 's2'},
+                                        's2':
+                                        {'advance': 'set_backend',
+                                         'parameters': {
+                                             'backend': 'chef-server',
+                                             'backend_state': 'installed'}}}}),
          'language': 'json',
          'backend': 'unprovisioned',
          'backend_state': 'unknown'}]
