102858b7b7b4cb390e813d8e9b1ec8ad2fe8a4b7
galstrom21 <jshepher@rackspace.com>
2012-10-04 16:47:53 -0500

    Moving test runner over to nose.. and adding coverage option

--- run_tests.sh
+++ run_tests.sh
@@ -8,6 +8,7 @@ function usage {
   echo "  -N, --no-virtual-env     Don't use virtualenv.  Run tests in local environment"
   echo "  -f, --force              Force a clean re-build of the virtual environment. Useful when dependencies have been added."
   echo "  -p, --pep8               Just run pep8"
+  echo "  -c, --coverage           Generate coverage report"
   echo "  -h, --help               Print this usage message"
   echo ""
   echo "Note: with no options specified, the script will try to run the tests in a virtual environment,"
@@ -16,29 +17,60 @@ function usage {
   exit
 }
 
+function process_option {
+  case "$1" in
+    -h|--help) usage;;
+    -V|--virtual-env) always_venv=1; never_venv=0;;
+    -N|--no-virtual-env) always_venv=0; never_venv=1;;
+    -f|--force) force=1;;
+    -p|--pep8) just_pep8=1;;
+    -c|--coverage) coverage=1;;
+    -*) noseopts="$noseopts $1";;
+    *) noseargs="$noseargs $1"
+  esac
+}
+
 venv=.roush-venv
 with_venv=tools/with_venv.sh
 always_venv=0
 never_venv=0
 force=0
+#no_site_packages=0
+# installvenvopts=
+noseargs=
+noseopts="-v"
 wrapper=""
 just_pep8=0
+coverage=0
 
-while [ ${#@} -gt 0 ]; do
-  case "$1" in
-    -h|--help) usage;;
-    -V|--virtual-env) let always_venv=1; let never_venv=0;;
-    -N|--no-virtual-env) let always_venv=0; let never_venv=1;;
-    -f|--force) let force=1;;
-    -p|--pep8) let just_pep8=1
-  esac
 
-  shift
+for arg in "$@"; do
+  process_option $arg
 done
 
+# If enabled, tell nose to collect coverage data
+if [ $coverage -eq 1 ]; then
+    noseopts="$noseopts --with-coverage --cover-package=webapp"
+    noseopts="$noseopts --cover-package=db"
+    noseopts="$noseopts --cover-package=backends"
+fi
+
 function run_tests {
+  # Cleanup *pyc
+  ${wrapper} find . -type f -name "*.pyc" -delete
   # Just run the test suites in current environment
-  ${wrapper} $TESTS
+  ${wrapper} $NOSETESTS
+  RESULT=$?
+  if [ "$RESULT" -ne "0" ];
+  then
+    exit 1
+  #  ERRSIZE=`wc -l run_tests.log | awk '{print \$1}'`
+  #  if [ "$ERRSIZE" -lt "40" ];
+  #  then
+  #      cat run_tests.log
+  #  fi
+  fi
+  return $RESULT
 }
 
 function run_pep8 {
@@ -48,7 +80,9 @@ function run_pep8 {
   PEP8_INCLUDE="."
   ${wrapper} pep8 $PEP8_OPTIONS $PEP8_INCLUDE || exit 1
 }
-TESTS="env python test_runner.py"
+
+
+NOSETESTS="nosetests $noseopts $noseargs tests/*.py"
 
 if [ $never_venv -eq 0 ]
 then
@@ -70,10 +104,15 @@ then
       if [ "x$use_ve" = "xY" -o "x$use_ve" = "x" -o "x$use_ve" = "xy" ]; then
         # Install the virtualenv and run the test suite in it
         env python tools/install_venv.py
-                    wrapper=${with_venv}
+        wrapper=${with_venv}
       fi
     fi
   fi
+fi
+
+# Delete old coverage data from previous runs
+if [ $coverage -eq 1 ]; then
+    ${wrapper} coverage erase
 fi
 
 if [ $just_pep8 -eq 1 ]; then
@@ -81,4 +120,12 @@ if [ $just_pep8 -eq 1 ]; then
     exit
 fi
 
-run_tests || exit
+# run_tests || exit
+run_tests
+
+if [ $coverage -eq 1 ]; then
+    echo "Generating coverage report in coverage/"
+    # Don't compute coverage for common code, which is tested elsewhere
+    # ${wrapper} coverage html --include='nova/*' --omit='nova/openstack/common/*' -d coverage -i
+    ${wrapper} coverage html --include='webapp/*,db/*,backends/*' -d coverage -i
+fi
