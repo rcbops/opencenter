861a1ed3573a4fcbc4a520a4a03b509a30d4aa88
rpedde <ron@pedde.com>
2012-11-18 13:29:46 -0600

    test inversions and assignments on expressions

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -23,6 +23,15 @@ class ExpressionTestCase(RoushTestCase):
         root_node = builder.build()
         return root_node.eval_node(node, symbol_table=ns)
 
+    def _invert_expression(self, expression, ns={}):
+        builder = ast.FilterBuilder(ast.FilterTokenizer(), expression)
+        root_node = builder.build()
+        return root_node.invert()
+
+    def _eval_expression(self, expression, node, ns={}):
+        builder = ast.FilterBuilder(ast.FilterTokenizer(), expression)
+        return builder.eval_node(node, symbol_table=ns)
+
     def test_bad_interface(self):
         expression = "ifcount('blahblah') > 0"
         self.assertRaises(SyntaxError, self._run_expression,
@@ -41,3 +50,42 @@ class ExpressionTestCase(RoushTestCase):
         result = self._run_expression(self.nodes['node-1'], expression)
         self.logger.debug('Got result: %s' % result)
         self.assertEquals(result, True)
+
+    def test_invert_equlas(self):
+        expression = "facts.test = 'test'"
+        result = self._invert_expression(expression)
+        self.assertEquals(result, ["facts.test := 'test'"])
+
+    def test_invert_and(self):
+        expression = "facts.test='test' and facts.x='x'"
+        result = self._invert_expression(expression)
+        self.assertTrue("facts.test := 'test'" in result)
+        self.assertTrue("facts.x := 'x'" in result)
+
+    def test_invert_in(self):
+        expression = "'test' in facts.foo"
+        result = self._invert_expression(expression)
+        self.assertTrue("facts.foo := union(facts.foo, 'test')" in result)
+        self.assertTrue(len(result) == 1)
+
+    def test_eval_assign(self):
+        expression = "foo := 3"
+        test_node = {}
+        self._eval_expression(expression, test_node)
+        self.assertTrue(test_node['foo'] == 3)
+        self.assertTrue(len(test_node) == 1)
+
+    def test_eval_union(self):
+        expression = "foo := union(foo, 3)"
+        test_node = {}
+        self._eval_expression(expression, test_node)
+        self.assertTrue(test_node['foo'] == [3])
+        self.assertTrue(len(test_node) == 1)
+
+    def test_eval_namespaces(self):
+        expression = "foo := value"
+        test_node = {}
+        ns = {"value": 3}
+        self._eval_expression(expression, test_node, ns)
+        self.assertTrue(test_node['foo'] == 3)
+        self.assertTrue(len(test_node) == 1)
