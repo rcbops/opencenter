1bec2e73cbe757761a460bb1bc43bc3e9e6bf452
rpedde <ron@pedde.com>
2012-11-29 09:56:44 -0600

    package updates

--- setup.py
+++ setup.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 
-import setuptools
+from setuptools import setup, find_packages
+
+requirements = ['flask', 'sqlalchemy', 'sqlalchemy-migrate', 'gevent']
 
 
-setuptools.setup(name='roush')
+setup(name='roush',
+      version='1.0.0',
+      description='Roush API server',
+      author='Justin Shepherd',
+      author_email='jshephar@rackspace.com',
+      url='',
+      license='Apache',
+      packages=find_packages(exclude=['tests', 'tests.*']),
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intented Audience :: Information Technology',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independant',
+        'Programming Language :: Python',
+        ],
+      install_requires=requirements,
+      entry_points={
+        'console_scripts': ['roush = roush:main']
+        }
+      )
