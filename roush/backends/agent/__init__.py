6e20f1ad3a21775e40127efd87fc8a2c462ffdf7
rpedde <ron@pedde.com>
2013-01-02 18:05:29 -0600

    add run_task primitive to agent backend

---
+++ roush/backends/agent/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import time
+import roush
+
+
+class AgentBackend(roush.backends.Backend):
+    def __init__(self):
+        super(AgentBackend, self).__init__(__file__)
+
+    def additional_constraints(self):
+        # this is probably a bit viscious.
+        return None
+
+    def run_task(self, api, node_id, **kwargs):
+        action = kwargs['action']
+        payload = kwargs['payload']
+
+        task = api._model_create('tasks', {'node_id': node_id,
+                                           'action': 'sleep', # action
+                                           'payload': payload})
+
+        self.logger.debug('added task as id %s' % task['id'])
+
+        while task['state'] not in ['timeout', 'cancelled', 'done']:
+            time.sleep(5)
+            task = api._model_get_by_id('tasks', task['id'])
+
+        return True
