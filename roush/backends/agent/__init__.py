a5d5a6b014a2d4107ecb070d3fa2ca0dff00e573
rpedde <ron@pedde.com>
2013-01-03 09:09:02 -0600

    pep8

--- roush/backends/agent/__init__.py
+++ roush/backends/agent/__init__.py
@@ -17,7 +17,7 @@ class AgentBackend(roush.backends.Backend):
         payload = kwargs['payload']
 
         task = api._model_create('tasks', {'node_id': node_id,
-                                           'action': 'sleep', # action
+                                           'action': action,
                                            'payload': payload})
 
         self.logger.debug('added task as id %s' % task['id'])
