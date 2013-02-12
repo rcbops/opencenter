09e00200079bbae3a9d40602e69010b92182786b
wilk <william.kelly@rackspace.com>
2013-02-12 15:12:15 -0600

    return json on authentication message

--- roush/webapp/auth.py
+++ roush/webapp/auth.py
@@ -35,8 +35,7 @@ def check_auth(username, password, roles):
 
 def authenticate():
     return Response(
-        'Could not verify your access level for that URL.\n'
-        'You have to login with proper credentials', 401,
+        '{"msg": "unauthorized"}', 401,
         {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
 
