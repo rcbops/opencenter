4073c35ddd16b356bf514c269990f0f7f336f6b9
git-harry <git-harry@live.co.uk>
2013-02-22 11:18:35 +0000

    New adventure tests to improve coverage.

--- tests/test_adventures.py
+++ tests/test_adventures.py
@@ -1,10 +1,34 @@
-#
-
-from util import RoushTestCase
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+from util import _test_request_returns, _test_seed_data_request_returns
 from util import inject
+from util import RoushTestCase
 
 
 class AdventuresTests(RoushTestCase):
     base_object = 'adventure'
 
-AdventuresTests = inject(AdventuresTests)
+
+def build_tests():
+    ats = inject(AdventuresTests)
+
+    test = lambda self: _test_request_returns(
+        self, 'post', '/%s/1/execute' % self._pluralize(self.base_object),
+        {}, 400)
+    test.__name__ = 'test_adventure_execute_no_node_returns_400'
+    setattr(ats, test.__name__, test)
+
+    test = lambda self: _test_seed_data_request_returns(
+        self, 'post', '/%s/1/execute' % self._pluralize(self.base_object),
+        {'node': 9999999}, 404, {self.base_object: 1})
+    test.__name__ = 'test_adventure_execute_non_existant_node_returns_404'
+    setattr(ats, test.__name__, test)
+
+    test = lambda self: _test_seed_data_request_returns(
+        self, 'post', '/%s/999999/execute' % self._pluralize(self.base_object),
+        {'node': 1}, 404, {'node': 1})
+    test.__name__ = 'test_adventure_execute_non_existant_adventure_returns_404'
+    setattr(ats, test.__name__, test)
+
+    return ats
+
+AdventuresTests = build_tests()
